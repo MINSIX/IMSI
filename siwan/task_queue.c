53e57f0e3b93c7de130c31b804bfb9284fc16adf
cissly <tkdwls237@naver.com>
2024-12-10 16:51:16 +0900

    queue update

--- siwan/task_queue.c
+++ siwan/task_queue.c
@@ -7,6 +7,8 @@ void initQueue(TaskQueue* q) {
     q->front = -1;
     q->rear = -1;
     pthread_mutex_init(&q->mutex, NULL); // mutex 초기화
+    pthread_cond_init(&q->Empty,NULL);
+    pthread_cond_init(&q->Full,NULL);
 }
 
 // 큐가 비어있는지 확인
@@ -22,18 +24,18 @@ int isFull(TaskQueue* q) {
 // 큐에 요소 추가
 void enqueue(TaskQueue* q, void* value) {
     pthread_mutex_lock(&q->mutex);
-
-    if (isFull(q)) {
+    
+    while(isFull(q)) {
         printf("큐가 가득 찼습니다!\n");
-        pthread_mutex_unlock(&q->mutex); 
-        return;
+        pthread_cond_wait(&q->Empty,&q->mutex); 
+        
     }
     if (isEmpty(q)) {
         q->front = 0; // 첫 번째 요소 추가 시 front 초기화
     }
     q->rear++;
     q->task[q->rear] = value;
-
+    pthread_cond_signal(&q->Full);
     pthread_mutex_unlock(&q->mutex); 
 }
 
@@ -41,17 +43,16 @@ void enqueue(TaskQueue* q, void* value) {
 void* dequeue(TaskQueue* q) {
     pthread_mutex_lock(&q->mutex); // mutex lock
 
-    if (isEmpty(q)) {
+    while (isEmpty(q)) {
         printf("큐가 비어있습니다!\n");
-        pthread_mutex_unlock(&q->mutex); // mutex lock 
-        return NULL; // 오류 코드
+        pthread_cond_wait(&q->Full,&q->mutex); // mutex lock 
     }
     void* item = q->task[q->front];
     q->front++;
     if (q->front > q->rear) { // 큐가 비어있게 되면 초기화
         q->front = q->rear = -1;
     }
-
+    pthread_cond_signal(&q->Empty);
     pthread_mutex_unlock(&q->mutex); // mutex lock
     return item;
 
